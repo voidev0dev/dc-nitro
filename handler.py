c36931ad57cda7ae427cb2c46443f4be8b3cf605
voidev <voidevd451@gmail.com>
2025-01-29 18:36:05 +0300

    Add files via upload

--- handler.py
+++ handler.py
@@ -2,7 +2,5 @@ from nitrogenerator import *
 import threading
 
 if __name__ == "__main__":
-    threads = []
-    for i in range(5):
+    for i in range(20):
         t = threading.Thread(target=main, args=(i,)).start()
-        threads.append(t)
